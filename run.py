c063dfc4f6cb9ea201c0acbe5ba2c1c944f5f402
Devanshu Gupta <devu@Devanshus-MacBook-Air.local>
2024-07-02 20:20:02 -0700

    commit 3

--- run.py
+++ run.py
@@ -108,7 +108,7 @@ def run_cv(args, wandb_args):
         wandb_run = None
         c = args
     else:
-        wandb_run = wandb.init(**wandb_args, mode = 'offline')
+        wandb_run = wandb.init(**wandb_args)
         args.cv_index = 0
         wandb.config.update(args, allow_val_change=True)
         c = wandb.config
